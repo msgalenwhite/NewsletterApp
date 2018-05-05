d7c4cf9a9d64eada198ebabd55b59dc4b8209123
Galen <galen.h.white@gmail.com>
2018-05-05 19:47:10 -0400

    added and tested validations (required new gem: SHOULDA)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, :type => :model do
+  context 'validations' do
+    it { should have_valid(:first_name).when("name") }
+    it { should_not have_valid(:first_name).when(nil, "") }
+    it { should have_valid(:last_name).when("name") }
+    it { should_not have_valid(:last_name).when(nil, "") }
+
+    it { should have_valid(:pic_url).when("url") }
+    it { should_not have_valid(:pic_url).when(nil, "") }
+
+    it { should have_valid(:bio).when("bio") }
+    it { should_not have_valid(:bio).when(nil, "") }
+
+    it { should have_valid(:current_city).when("city") }
+    it { should_not have_valid(:current_city).when(nil, "") }
+
+    it { should have_valid(:current_state).when("state") }
+    it { should_not have_valid(:current_state).when(nil, "") }
+  end
 end
