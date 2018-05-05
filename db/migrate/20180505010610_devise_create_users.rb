d8ee811624126d7a4a15183626fa7c0d9124a891
Galen <galen.h.white@gmail.com>
2018-05-05 19:26:05 -0400

    updated migration

--- db/migrate/20180505010610_devise_create_users.rb
+++ db/migrate/20180505010610_devise_create_users.rb
@@ -32,8 +32,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
+
+      # Not added by devise:
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :pic_url, null: false
+
+      t.text :bio, null: false
+
+      t.string :current_city, null: false
+      t.string :current_state, null: false
     end
 
     add_index :users, :email,                unique: true
