7aa9ba1661bf561ce7801bb7a861ad860ab9f067
Galen <galen.h.white@gmail.com>
2018-05-05 19:37:28 -0400

    set up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :pic_url, presence: true
+  validates :bio, presence: true
+  validates :current_city, presence: true
+  validates :current_state, presence: true
 end
