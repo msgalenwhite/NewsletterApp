387224c02e305743e00493834211e1421e2f07f8
Galen <galen.h.white@gmail.com>
2018-05-06 13:05:44 -0400

    added test for userhomepage back in

---
+++ spec/javascript/UserHomePage.js
@@ -0,0 +1,25 @@
+import UserHomePage from '../../app/javascript/containers/UserHomePage'
+
+describe ('UserHomePage', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<UserHomePage />)
+  })
+
+  // Testing State and its changes
+  it ('initializes with the correct state', () => {
+    expect(wrapper.state()).toEqual({
+      newsletters: []
+    })
+  })
+
+  // Testing rendered components
+  it('contains a header', () => {
+    expect(wrapper.text()).toContain('Newsletter Home Page')
+  })
+  it('has a "link" to create a Newsletter', () => {
+    expect(wrapper.text()).toContain('Create a Newsletter')
+    expect(wrapper.find('a').text()).toContain('Create a Newsletter')
+  })
+})
