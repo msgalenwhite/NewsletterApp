c09556dc00d0e41113423340df1cd335a36d620d
Galen <galen.h.white@gmail.com>
2018-05-07 17:40:33 -0400

    commented out fetch to work on tomorrow

--- app/javascript/containers/EntryFormContainer.js
+++ app/javascript/containers/EntryFormContainer.js
@@ -41,21 +41,21 @@ class EntryFormContainer extends Component {
   }
 
   submitEntry() {
-    fetch("/api/v1/entries.json")
-      .then ( response => {
-        if ( response.ok ) {
-          return response;
-        } else {
-          let errorMessage = `${response.status} (${response.statusText})`;
-          let error = new Error(errorMessage);
-          throw(error);
-        }
-      })
-      .then ( response => response.json() )
-      .then ( response => {
-        console.log(response)
-      })
-      .catch ( error => console.error(`Error in fetch: ${error.message}`) );
+    // fetch("/api/v1/entries.json")
+    //   .then ( response => {
+    //     if ( response.ok ) {
+    //       return response;
+    //     } else {
+    //       let errorMessage = `${response.status} (${response.statusText})`;
+    //       let error = new Error(errorMessage);
+    //       throw(error);
+    //     }
+    //   })
+    //   .then ( response => response.json() )
+    //   .then ( response => {
+    //     console.log(response)
+    //   })
+    //   .catch ( error => console.error(`Error in fetch: ${error.message}`) );
   }
 
   render() {
