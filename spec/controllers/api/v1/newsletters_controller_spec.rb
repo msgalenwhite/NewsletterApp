4d48d4c04e9fbf24820bbdd9215c4066ff0934e7
Galen <galen.h.white@gmail.com>
2018-05-07 14:14:39 -0400

    added additional devise test helpers

--- spec/controllers/api/v1/newsletters_controller_spec.rb
+++ spec/controllers/api/v1/newsletters_controller_spec.rb
@@ -2,24 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::NewslettersController, type: :controller do
 
-  user = FactoryBot.create(:user)
+  @user = FactoryBot.create(:user)
   news = FactoryBot.create(:newsletter)
   news_two = FactoryBot.create(:newsletter)
 
-  Subscription.create(user: user, newsletter: news)
-  Subscription.create(user: user, newsletter: news_two)
+  Subscription.create(user: @user, newsletter: news)
+  Subscription.create(user: @user, newsletter: news_two)
 
-  current_user = user
-
-  describe 'GET#index' do
+  xdescribe 'GET#index' do
     it "should return a list of newsletters" do
+      sign_in @user
       get :index
       returned_json = JSON.parse(response.body)
 
-      binding.pry
-
       expect(response.status).to eq(200)
       expect(response.content_type).to eq("application/json")
+
+      binding.pry
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,4 +59,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+
 end
