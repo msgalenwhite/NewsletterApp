ddf13b1f27b996eaf2528ba8ddb1ad34447e7788
Galen <galen.h.white@gmail.com>
2018-05-07 18:04:59 -0400

    styled newsletter tile

--- app/javascript/components/NewsletterDisplayTile.js
+++ app/javascript/components/NewsletterDisplayTile.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 const NewsletterDisplayTile = props => {
   return(
-    <div>
-      <h4>{props.title}</h4>
-      <p>{props.description}</p>
-      <img src={props.pic_url} alt='Newsletter Thumbnail' />
+    <div className='newsletter-display-tile'>
+      <div className='opaque-tile row'>
+        <span className='columns small-5'>
+          <img src={props.pic_url} alt='Newsletter Thumbnail' />
+        </span>
+        <span className='columns small-7'>
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+        </span>
+      </div>
     </div>
   )
 }
