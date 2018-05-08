c005b84dbf2a42638cd337dde8e2c530b9c3fb73
Galen <galen.h.white@gmail.com>
2018-05-08 19:53:40 -0400

    removed user_id from backend

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -2,18 +2,18 @@ class Api::V1::EntriesController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def create
-    @entry = Entry.new(entries_params)
+    entry = Entry.new(entries_params)
 
-    if @entry.save
-      render json: @entry
+    if entry.save
+      render json: entry
     else
-      render json: @entry.errors.full_messages.join(" // ")
+      render json: entry.errors.full_messages.join(" // ")
     end
   end
 
   private
 
   def entries_params
-    params.require(:entry).permit(:title, :body, :user_id, :newsletter_id)
+    params.require(:entry).permit(:title, :body, :newsletter_id)
   end
 end
