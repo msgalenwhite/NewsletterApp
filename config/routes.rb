20eff25296d2bd0496795575a6b85978d61afef1
Galen <galen.h.white@gmail.com>
2018-05-08 22:51:20 -0400

    added a route for api as well as view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :newsletters, only: [:index]
+      resources :newsletters, only: [:index, :show]
       resources :entries, only: [:create]
     end
   end
