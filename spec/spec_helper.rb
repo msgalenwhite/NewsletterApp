d710395fefe0461004e3c1147feec1edc6b6791c
Galen <galen.h.white@gmail.com>
2018-05-10 12:15:49 -0400

    Revert "emails haven't been tested, but basic infrastructure is there"
    This reverts commit 1872e514f3505fe1e11e1042dfd1d8fcbda54f9c.
    
    Want to go back and follow a simpler procedure for emails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,8 +96,4 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-
-  config.before :each do
-    ActionMailer::Base.deliveries.clear
-  end
 end
