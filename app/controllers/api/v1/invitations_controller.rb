811be6a0df72ac895f4fd4a471eca632640670d2
Galen <galen.h.white@gmail.com>
2018-05-10 15:15:19 -0400

    cleaned out file ready to start

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::InvitationsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def create
-    binding.pry
+
+    # <ActionController::Parameters {"emails"=>[{"email"=>"sdef@email.com", "name"=>"sdef"}], "newsletterId"=>1, "controller"=>"api/v1/invitations", "action"=>"create", "format"=>"json", "invitation"=>{}} permitted: false>
+
+    flash[:alert] = "We're still working on it"
   end
 end
