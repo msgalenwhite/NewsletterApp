ea747b90634404f475cb1e946515c20bb547ee60
Galen <galen.h.white@gmail.com>
2018-05-10 16:50:52 -0400

    maybe in the right direction

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -2,9 +2,29 @@ class Api::V1::InvitationsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def create
+    binding.pry
+    # => <ActionController::Parameters {"emails"=>[{"email"=>"feswd@test.com", "name"=>"ljkgsdewr"}], "newsletterId"=>1, "controller"=>"api/v1/invitations", "action"=>"create", "format"=>"json", "invitation"=>{}} permitted: false>
+    response = []
 
-    # <ActionController::Parameters {"emails"=>[{"email"=>"sdef@email.com", "name"=>"sdef"}], "newsletterId"=>1, "controller"=>"api/v1/invitations", "action"=>"create", "format"=>"json", "invitation"=>{}} permitted: false>
+    params["emails"].each do |data_hash|
+      @invitation = Invitation.new(
+        host_id: current_user.id,
+        newsletter_id: data_hash["newsletterId"],
+        email: data_hash["email"],
+        name: data_hash["name"]
+      )
 
-    flash[:alert] = "We're still working on it"
+      if @invitation.save
+        InvitationMailer.send_invite(@invitation).deliver_now
+
+        response << { email: @invitation.email, name: @invitation.name }
+
+        flash[:alert] = "We're still working on it"
+      end
+
+      redirect_to root_path
+    end
+
+    render json: response
   end
 end
