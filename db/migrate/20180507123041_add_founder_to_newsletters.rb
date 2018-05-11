b23206f61d3ca53da9f93d20f49afa4956d0774f
Galen <galen.h.white@gmail.com>
2018-05-11 16:53:36 -0400

    changed founder to founder_id

--- db/migrate/20180507123041_add_founder_to_newsletters.rb
+++ db/migrate/20180507123041_add_founder_to_newsletters.rb
@@ -1,5 +1,5 @@
 class AddFounderToNewsletters < ActiveRecord::Migration[5.2]
   def change
-    add_column :newsletters, :founder, :integer, null: false
+    add_column :newsletters, :founder_id, :integer, null: false
   end
 end
