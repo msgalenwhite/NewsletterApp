c9787b8d38160027a841a3fc6754c9d85fbc2081
Galen <galen.h.white@gmail.com>
2018-05-11 08:17:20 -0400

    backend now responsible for creating entries in invitation table and returning failed invitations

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -4,25 +4,21 @@ class Api::V1::InvitationsController < ApplicationController
   def create
     binding.pry
     # => <ActionController::Parameters {"emails"=>[{"email"=>"feswd@test.com", "name"=>"ljkgsdewr"}], "newsletterId"=>1, "controller"=>"api/v1/invitations", "action"=>"create", "format"=>"json", "invitation"=>{}} permitted: false>
-    response = []
+    failures = []
 
     params["emails"].each do |data_hash|
       @invitation = Invitation.new(
         host_id: current_user.id,
-        newsletter_id: data_hash["newsletterId"],
+        newsletter_id: params["newsletterId"],
         email: data_hash["email"],
         name: data_hash["name"]
       )
 
       if @invitation.save
-        InvitationMailer.send_invite(@invitation).deliver_now
 
-        response << { email: @invitation.email, name: @invitation.name }
-
-        flash[:alert] = "We're still working on it"
+      else
+        @failures << @invitation
       end
-
-      redirect_to root_path
     end
 
     render json: response
