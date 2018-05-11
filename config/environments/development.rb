867dc736389bbd70e4838e1f493ec9987024d3d5
Galen <galen.h.white@gmail.com>
2018-05-11 12:42:17 -0400

    foundation loads first so I can override it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,21 +57,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # MAILCATCHER CONFIG
-  #   to view emails generated in development:
-  #   `gem install mailcatcher && mailcatcher`
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: "localhost",
-    port: 1025
-  }
-
-  config.action_mailer.default_url_options = {
-    host: "localhost",
-    port: 3000
-  }
-
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
