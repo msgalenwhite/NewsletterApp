f430c32ea38ade5c258fdcb54a4e04dc36a8c41c
Galen <galen.h.white@gmail.com>
2018-05-12 11:39:28 -0400

    added entry show page as its own path

---
+++ app/javascript/entryTileComponents/FullEntry.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FullEntry = props => {
+  //will also show pictures
+  return(
+    <div>
+      <p>{props.text}</p>
+    </div>
+  )
+}
+
+export default FullEntry
--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -3,12 +3,14 @@ import { browserHistory, Router, Route, IndexRoute } from 'react-router'
 
 import UserHomePage from '../pagecontainers/UserHomePage'
 import NewsletterShowPage from '../pagecontainers/NewsletterShowPage'
+import EntryShowPage from '../pagecontainers/EntryShowPage'
 
 const App = props => {
 
   return(
     <Router history={browserHistory} >
       <Route path='/' component={UserHomePage} />
+      <Route path='/newsletters/:newsletter_id/entries/:entry_id' component={EntryShowPage} />
       <Route path='/newsletters/:id' component={NewsletterShowPage} />
     </Router>
   )
