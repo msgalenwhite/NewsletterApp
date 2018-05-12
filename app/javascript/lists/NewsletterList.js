cc37dbb55a299dd0527f564b8e27555079db9fa3
Galen <galen.h.white@gmail.com>
2018-05-12 12:02:58 -0400

    simplified NewsletterList

--- app/javascript/lists/NewsletterList.js
+++ app/javascript/lists/NewsletterList.js
@@ -1,51 +1,138 @@
 import React from 'react'
+// newsletters={this.state.subscribedNewsletters}
+// showForm={this.displayOrHideForm}
+// newsletterNeedingEntry={this.state.newsletterNeedingEntry}
+// setMessage={this.setMessage}
+// displayOrHideNewsletterInfo={this.displayOrHideNewsletterInfo}
+// selectedNewsletter={this.state.selectedNewsletter}
 import NewsletterDisplayTile from '../newsletterTileComponents/NewsletterDisplayTile'
 
-const NewsletterList = props => {
-  let newsletters;
+import React, {Component} from 'react'
 
-  if (props.newsletters.length > 0) {
-    newsletters = props.newsletters.map((newsletterObject) => {
-      let showEntryForm;
-      let showNewsletterBoolean;
-      const id = newsletterObject["id"]
+class NewsletterList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newsletters: [],
+      openNewsletter: null
+    }
+    this.generateNewsletterTiles = this.generateNewsletterTiles.bind(this)
+    this.openANewsletter = this.openANewsletter.bind(this)
+  }
 
-      if (id === props.newsletterNeedingEntry) {
-        showEntryForm = true
+  componentDidMount() {
+    fetch("/api/v1/newsletters.json", {
+      credentials: 'same-origin',
+      method: 'GET',
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+    })
+      .then ( response => {
+        if ( response.ok ) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`;
+          let error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then ( response => response.json() )
+      .then ( response => {
+        this.setState({ newsletters: response })
+      })
+      .catch ( error => console.error(`Error in fetch: ${error.message}`) );
+  }
+
+  generateNewsletterTiles() {
+    const tiles = this.state.newsletters.map((newsletter) => {
+
+      let isOpen;
+      if (newsletter.id === this.state.openNewsletter) {
+        isOpen = true;
       }
-      if (id === props.selectedNewsletter) {
-        showNewsletterBoolean = true
-      }
 
-      const handleSelectNewsletter = () => {
-        props.displayOrHideNewsletterInfo(id)
+      const openMe = () => {
+        this.openANewsletter(newsletter.id)
       }
-
-      const handleButtonClick = () => { props.showForm(id) }
 
       return (
         <NewsletterDisplayTile
-          key={id}
-          id={id}
-          title={newsletterObject["title"]}
-          description={newsletterObject["description"]}
-          pic_url={newsletterObject["thumb_photo"]["url"]}
-          handleClick={handleButtonClick}
-          showEntryForm={showEntryForm}
-          userId={props.userId}
-          setMessage={props.setMessage}
-          showDetails={handleSelectNewsletter}
-          showNewsletterBoolean={showNewsletterBoolean}
+          details={newsletter}
+          isOpen={isOpen}
+          openMe={openMe}
+          setMessage={this.props.setMessage}
         />
       )
     })
   }
 
-  return(
-    <ul>
-      {newsletters}
-    </ul>
-  )
+  openANewsletter(id) {
+    if (id === this.state.openNewsletter) {
+      this.setState({ openNewsletter: null })
+    } else {
+      this.setState({ openNewsletter: id })
+    }
+  }
+
+  render() {
+    const tiles = this.generateNewsletterTiles()
+
+    return(
+      <ul>
+        {tiles}
+      </ul>
+    )
+  }
 }
 
 export default NewsletterList
+
+
+//
+// const NewsletterList = props => {
+//   let newsletters;
+//
+//   if (props.newsletters.length > 0) {
+//     newsletters = props.newsletters.map((newsletterObject) => {
+//       let showEntryForm;
+//       let showNewsletterBoolean;
+//       const id = newsletterObject["id"]
+//
+//       if (id === props.newsletterNeedingEntry) {
+//         showEntryForm = true
+//       }
+//       if (id === props.selectedNewsletter) {
+//         showNewsletterBoolean = true
+//       }
+//
+//       const handleSelectNewsletter = () => {
+//         props.displayOrHideNewsletterInfo(id)
+//       }
+//
+//       const handleButtonClick = () => { props.showForm(id) }
+//
+//       return (
+//         <NewsletterDisplayTile
+//           key={id}
+//           id={id}
+//           title={newsletterObject["title"]}
+//           description={newsletterObject["description"]}
+//           pic_url={newsletterObject["thumb_photo"]["url"]}
+//           handleClick={handleButtonClick}
+//           showEntryForm={showEntryForm}
+//           userId={props.userId}
+//           setMessage={props.setMessage}
+//           showDetails={handleSelectNewsletter}
+//           showNewsletterBoolean={showNewsletterBoolean}
+//         />
+//       )
+//     })
+//   }
+//
+//   return(
+//     <ul>
+//       {newsletters}
+//     </ul>
+//   )
+// }
+//
+// export default NewsletterList
--- app/javascript/pagecontainers/UserHomePage.js
+++ app/javascript/pagecontainers/UserHomePage.js
@@ -6,74 +6,56 @@ class UserHomePage extends Component {
   constructor(props){
     super(props);
     this.state = {
-      subscribedNewsletters: [],
-      newsletterNeedingEntry: null,
-      selectedNewsletter: null,
+      // subscribedNewsletters: [],
+      // newsletterNeedingEntry: null,
+      // selectedNewsletter: null,
       flashMessage: null
     }
-    this.displayOrHideForm = this.displayOrHideForm.bind(this)
-    this.displayOrHideNewsletterInfo = this.displayOrHideNewsletterInfo.bind(this)
+    // this.displayOrHideForm = this.displayOrHideForm.bind(this)
+    // this.displayOrHideNewsletterInfo = this.displayOrHideNewsletterInfo.bind(this)
     this.setMessage = this.setMessage.bind(this)
   }
 
-  displayOrHideForm(id) {
-    if (id === this.state.newsletterNeedingEntry) {
-      this.setState({ newsletterNeedingEntry: null })
-    } else {
-      this.setState({ newsletterNeedingEntry: id })
-    }
-  }
 
-  displayOrHideNewsletterInfo(id) {
-    if (id === this.state.selectedNewsletter) {
-      this.setState({
-        selectedNewsletter: null,
-        newsletterNeedingEntry: null
-      })
-    } else {
-      this.setState({ selectedNewsletter: id })
-    }
-  }
 
   setMessage(message) {
-    let id = this.state.newsletterNeedingEntry;
-    let letter = this.state.selectedNewsletter
-    if (message.includes("Success!")) {
-      id = null
-      letter = null
-    }
-    this.setState({
-      flashMessage: message,
-      newsletterNeedingEntry: id,
-      selectedNewsletter: letter
-    })
+    this.setState({ flashMessage: message })
   }
 
-  componentDidMount() {
-    fetch("/api/v1/newsletters.json", {
-      credentials: 'same-origin',
-      method: 'GET',
-      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
-    })
-      .then ( response => {
-        if ( response.ok ) {
-          return response;
-        } else {
-          let errorMessage = `${response.status} (${response.statusText})`;
-          let error = new Error(errorMessage);
-          throw(error);
-        }
-      })
-      .then ( response => response.json() )
-      .then ( response => {
-        const newsletters = response
+  // displayOrHideForm(id) {
+  //   if (id === this.state.newsletterNeedingEntry) {
+  //     this.setState({ newsletterNeedingEntry: null })
+  //   } else {
+  //     this.setState({ newsletterNeedingEntry: id })
+  //   }
+  // }
+  //
+  // displayOrHideNewsletterInfo(id) {
+  //   if (id === this.state.selectedNewsletter) {
+  //     this.setState({
+  //       selectedNewsletter: null,
+  //       newsletterNeedingEntry: null
+  //     })
+  //   } else {
+  //     this.setState({ selectedNewsletter: id })
+  //   }
+  // }
+  //
+  // setMessage(message) {
+  //   let id = this.state.newsletterNeedingEntry;
+  //   let letter = this.state.selectedNewsletter
+  //   if (message.includes("Success!")) {
+  //     id = null
+  //     letter = null
+  //   }
+  //   this.setState({
+  //     flashMessage: message,
+  //     newsletterNeedingEntry: id,
+  //     selectedNewsletter: letter
+  //   })
+  // }
 
-        this.setState({
-          subscribedNewsletters: newsletters
-        })
-      })
-      .catch ( error => console.error(`Error in fetch: ${error.message}`) );
-  }
+
 
   render() {
     return(
@@ -82,13 +64,8 @@ class UserHomePage extends Component {
           title='Your Subscriptions'
           flashMessage={this.state.flashMessage}/>
         <NewsletterList
-          newsletters={this.state.subscribedNewsletters}
-          showForm={this.displayOrHideForm}
-          newsletterNeedingEntry={this.state.newsletterNeedingEntry}
-          setMessage={this.setMessage}
-          displayOrHideNewsletterInfo={this.displayOrHideNewsletterInfo}
-          selectedNewsletter={this.state.selectedNewsletter}
-        />
+          newsletters={this.state.newsletters}
+          setMessage={this.setMessage} />
       </div>
     )
   }
