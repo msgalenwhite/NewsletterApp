ea3e91dcb311f320316ad266111050e719793659
Galen <galen.h.white@gmail.com>
2018-05-13 15:11:08 -0400

    added enzyme to imports

--- spec/javascript/testHelper.js
+++ spec/javascript/testHelper.js
@@ -1,4 +1,4 @@
-import { shallow, mount } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 import React from 'react';
 import 'jasmine-ajax';
