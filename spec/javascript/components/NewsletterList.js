8f925810289dcfd084078a78bd68b871c6276832
Galen <galen.h.white@gmail.com>
2018-05-13 17:34:49 -0400

    type in flash message

--- spec/javascript/components/NewsletterList.js
+++ spec/javascript/components/NewsletterList.js
@@ -1,65 +1,40 @@
-<<<<<<< HEAD:spec/javascript/lists/NewsletterList.js
-import NewsletterList from '../../../app/javascript/lists/NewsletterList'
-import NewsletterDisplayTile from '../../../app/javascript/newsletterTileComponents/NewsletterDisplayTile'
-import fetchMock from 'fetch-mock'
-=======
 import NewsletterList from '../../../app/javascript/components/NewsletterList'
 import NewsletterDisplayTile from '../../../app/javascript/components/NewsletterDisplayTile'
->>>>>>> parent of 8f124c4... restructured spec folders:spec/javascript/components/NewsletterList.js
 
 describe ('NewsletterList', () => {
   let wrapper;
-  let fetchedData;
+  let propData = [
+    {
+      "id": 1,
+      "description": "desc",
+      "thumb_photo": {
+        "url": "https://newsletter-app-development.s3.amazonaws.com/uploads/newsletter/thumb_photo/1/blue_flowers.jpg"
+      },
+      "title": "New",
+      "founder": {
+        "id": 1,
+        "email": "test@test.com",
+        "created_at": "2018-05-07T16:26:11.940Z",
+        "updated_at": "2018-05-07T17:51:11.270Z",
+        "first_name": "test",
+        "last_name": "user",
+        "profile_photo": {
+          "url": "https://newsletter-app-development.s3.amazonaws.com/uploads/user/profile_photo/1/purple_flowers.jpg"
+        },
+      "bio": "bio",
+      "current_city": "Boston",
+      "current_state": "MA"
+      }
+    }
+  ]
 
   beforeEach(() => {
-    fetchedData = [
-      {
-        "description": "desc",
-        "id": 1,
-        "thumb_photo": {
-          "url": "https://newsletter-app-development.s3.amazonaws.com/uploads/newsletter/thumb_photo/1/blue_flowers.jpg"
-        },
-        "title": "New",
-        "founder_name": "test user"
-      },
-      {
-        "description": "desc2",
-        "id": 2,
-        "thumb_photo": {
-          "url": "https://newsletter-app-development.s3.amazonaws.com/uploads/newsletter/thumb_photo/1/blue_flowers.jpg"
-        },
-        "title": "Another",
-        "founder_name": "not the test user"
-      },
-    ]
-
-    fetchMock.get('/api/v1/newsletters.json', {
-      status: 200,
-      body: fetchedData
-    });
     wrapper = mount(
-      <NewsletterList setMessage={jasmine.any(Function)} />
+      <NewsletterList newsletters={propData}/>
     )
   })
 
-  afterEach(fetchMock.restore)
-
-  describe('component initial mount', () => {
-    it ('initializes with the correct state', () => {
-      expect(wrapper.state()).toEqual({
-        newsletters: [],
-        openNewsletter: null,
-        showForm: false
-      })
-    })
-
-    it ('renders a NewsletterDisplayTile for each item returned from the api call', (done) => {
-      setTimeout(() => {
-        expect(wrapper.find(NewsletterDisplayTile)).toBePresent()
-        expect(wrapper.find(NewsletterDisplayTile).length).toEqual(fetchedData.length)
-        done()
-      }, 0)
-    })
+  it ('renders a list of NewsletterDisplayTiles', () => {
+    expect(wrapper.find(NewsletterDisplayTile).length).toEqual(propData.length)
   })
-
 })
--- spec/javascript/containers/UserHomePage.js
+++ spec/javascript/containers/UserHomePage.js
@@ -1,32 +1,80 @@
-<<<<<<< HEAD:spec/javascript/pageContainers/UserHomePage.js
-import UserHomePage from '../../../app/javascript/pageContainers/UserHomePage'
-import HeaderBar from '../../../app/javascript/headerComponents/HeaderBar'
-import NewsletterList from '../../../app/javascript/lists/NewsletterList'
-=======
 import fetchMock from 'fetch-mock'
 
 import UserHomePage from '../../../app/javascript/containers/UserHomePage'
 import NewsletterList from '../../../app/javascript/components/NewsletterList'
->>>>>>> parent of 8f124c4... restructured spec folders:spec/javascript/containers/UserHomePage.js
 
 describe ('UserHomePage', () => {
   let wrapper;
+  let fetchedData;
 
   beforeEach(() => {
+    fetchedData = [
+      {
+        "description": "desc",
+        "thumb_photo": {
+          "url": "https://newsletter-app-development.s3.amazonaws.com/uploads/newsletter/thumb_photo/1/blue_flowers.jpg"
+        },
+        "title": "New",
+        "founder_name": "test user"
+      },
+      {
+        "description": "desc2",
+        "thumb_photo": {
+          "url": "https://newsletter-app-development.s3.amazonaws.com/uploads/newsletter/thumb_photo/1/blue_flowers.jpg"
+        },
+        "title": "Another",
+        "founder_name": "not the test user"
+      },
+    ]
+
+    fetchMock.get('/api/v1/newsletters.json', {
+      status: 200,
+      body: fetchedData
+    });
     wrapper = mount(<UserHomePage />)
   })
 
-  it ("initializes with the correct state", () => {
-    expect(wrapper.state()).toEqual({
-      flashMessage: null
+  afterEach(fetchMock.restore)
+
+  describe('fetch statement', () => {
+    it ('initializes with the correct state', () => {
+      expect(wrapper.state()).toEqual({
+        subscribedNewsletters: [],
+        newsletterNeedingEntry: null,
+        selectedNewsletter: null,
+        flashMessage: null
+      })
+    })
+
+    it ('changes state to reflect the content of an api call', (done) => {
+      setTimeout(() => {
+        expect(wrapper.state()).toEqual({
+          subscribedNewsletters: fetchedData,
+          newsletterNeedingEntry: null,
+          selectedNewsletter: null,
+          flashMessage: null
+        })
+        done();
+      }, 0)
+    })
+
+    it ('passes the correct props to NewsletterList', () => {
+      expect(wrapper.find(NewsletterList)).toHaveProp(
+        'newsletters',
+        wrapper.state('subscribedNewsletters')
+      )
     })
   })
 
-  it ("renders a Header Bar", () => {
-    expect(wrapper.find(HeaderBar)).toBePresent()
-  })
-
-  it ("renders a NewsletterList", () => {
-    expect(wrapper.find(NewsletterList)).toBePresent()
+  describe('rendered components', () => {
+    it('contains a header', () => {
+      expect(wrapper.text()).toContain('Your Subscriptions')
+    })
+    it('has a "link" to create a Newsletter', () => {
+      expect(wrapper.find('a').text()).toContain('Create a Newsletter')
+    })
+    it ('has a list of newsletters', () => {
+      expect(wrapper.find(NewsletterList)).toBePresent()
+    })
   })
 })
