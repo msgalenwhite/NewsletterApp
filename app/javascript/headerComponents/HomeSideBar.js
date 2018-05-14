e55513d996ae9c44c3eb3f4f7165d13bef647f83
Galen <galen.h.white@gmail.com>
2018-05-14 17:58:12 -0400

    resized newsletters

--- app/javascript/headerComponents/HomeSideBar.js
+++ app/javascript/headerComponents/HomeSideBar.js
@@ -5,9 +5,6 @@ const HomeSideBar = props => {
   return(
     <div className='sidebar'>
       <h5>What's next?</h5>
-      <a href='/'><button className='general-button'>
-        View Subscriptions
-      </button></a>
       <a href='/newsletters/new' ><button className='general-button'>
         Start a Newsletter
       </button></a>
