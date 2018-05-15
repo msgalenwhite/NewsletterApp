0526dd0a1444e260a33154793947ea2b632ccc14
Galen <galen.h.white@gmail.com>
2018-05-15 15:03:09 -0400

    changed domain

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -6,7 +6,7 @@
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'herokuapp.com',
     :enable_starttls_auto => true
   }
 # end
