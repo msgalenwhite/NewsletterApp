981dedf276d48ab9e6853871ccdf47b0517bdb43
Galen <galen.h.white@gmail.com>
2018-05-15 13:34:25 -0400

    form takes in laitude and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_164718) do
+ActiveRecord::Schema.define(version: 2018_05_14_182932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,8 @@ ActiveRecord::Schema.define(version: 2018_05_13_164718) do
     t.text "bio", null: false
     t.string "current_city", null: false
     t.string "current_state", null: false
-    t.integer "zipcode"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
