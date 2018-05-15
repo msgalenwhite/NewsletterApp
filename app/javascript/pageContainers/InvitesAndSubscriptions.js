910f445265d0c8c78c0c6f20c646938e60f77e58
Galen <galen.h.white@gmail.com>
2018-05-15 12:21:48 -0400

    added text to page

--- app/javascript/pageContainers/InvitesAndSubscriptions.js
+++ app/javascript/pageContainers/InvitesAndSubscriptions.js
@@ -90,8 +90,18 @@ class InvitesAndSubscriptions extends Component {
     return(
       <div className='page'>
         <HeaderBar
-          title="Invitations"
+          title="Thank you for coming to check out the Family Newsletter!"
           flashMessage={this.state.message}/>
+        <div className='description-div'>
+          <h3 className='sub-header'>A little about us:</h3>
+          <p className='description-text'>
+            The Family Newsletter is a tradition dating back to my grandmother.  When she first left home, she and all of her siblings would designate one person each month to collect letters from all of the siblings, then copy and compile the letters into a newsletter which was mailed back out to everyone.
+          </p>
+          <p className='description-text'>
+            On our site we want to foster the same sense of community, and you're in luck!  We have an invitation or two waiting for you.  If you are interested in joining any of the newsletters, please select the checkbox next to the name.
+          </p>
+        </div>
+
         <div className='invite-page'>
           <InviteList
             invites={this.state.inviteInfo}
