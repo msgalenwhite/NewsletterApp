edd294477ae3493cf243bbd3a7b26a369f26bdc9
Galen <galen.h.white@gmail.com>
2018-05-16 11:44:18 -0400

    fetch statement is getting the correct data for newsletter

--- app/controllers/api/v1/printed_newsletters_controller.rb
+++ app/controllers/api/v1/printed_newsletters_controller.rb
@@ -1,17 +1,26 @@
 class Api::V1::PrintedNewslettersController < ApplicationController
   def show
-    @newsletter = Newsletter.find(params["newsletter_id"].to_i)
-    date_info = params["id"].split("&")
-    @month = date_info[0][6, date_info[0].length].to_i
-    @year = date_info[1][5, date_info[1].length].to_i
+    newsletter_id = params["newsletter_id"].to_i
 
-    @entries = @newsletters.entries.with_year_and_month(@year, @month)
+    date_info = params["id"].split("&")
+    month = date_info[0][6, date_info[0].length].to_i
+    year = date_info[1][5, date_info[1].length].to_i
+
+
+    @entries = specific_entries(newsletter_id, year, month)
 
     returnObject = {
-      newsletter: @newsletter,
+      newsletter: Newsletter.find(newsletter_id),
       entries: @entries
     }
 
     render json: returnObject
+  end
+
+  private
+
+  def specific_entries(newsletter_id, year, month)
+    possibleEntries = Entry.with_year_and_month(year, month)
+    possibleEntries.select{ |entry| entry.newsletter_id = newsletter_id }
   end
 end
