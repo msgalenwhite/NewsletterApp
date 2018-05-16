e055df31f68563a8137805ce0e64d09fd71093a9
Galen <galen.h.white@gmail.com>
2018-05-16 09:23:33 -0400

    qrcode appears in popup

--- app/javascript/inviteListComponents/PopUp.js
+++ app/javascript/inviteListComponents/PopUp.js
@@ -8,10 +8,15 @@ const PopUp = props => {
   return(
     <div id="myModal" data-reveal data-options={buttonToClose} aria-labelledby="modalTitle" aria-hidden="true" role="dialog" className="reveal-modal text-center">
 
-      <h3>THIS IS A POPUP!</h3>
-      <QRCode value="https://familynewsletter.herokuapp.com/users/sign_in" />
+      <h3 className='sub-header center pop-up'>Thank you for inviting your friends to the Family Newsletter!</h3>
 
-      <button className='general-button' onClick={props.closeFunc}>
+      <p>Emailed invitations have already been sent, but you can also share this QR code to help your family find the site.  Just make sure that they create an account with the same email that you entered, and they will be able to join your newsletter.</p>
+
+      <div className='row center qr-code'>
+        <QRCode value="https://familynewsletter.herokuapp.com/users/sign_up" />
+      </div>
+
+      <button className='general-button center' onClick={props.closeFunc}>
         Back to Newsletters
       </button>
     </div>
