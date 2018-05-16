dccc1bfe0e9c4c3b500213af1180387dc0f1e3fb
Galen <galen.h.white@gmail.com>
2018-05-16 09:13:24 -0400

    qrcode appears

--- app/javascript/inviteListComponents/PopUp.js
+++ app/javascript/inviteListComponents/PopUp.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import QRCode from 'qrcode.react'
 
 const PopUp = props => {
 
@@ -8,6 +9,8 @@ const PopUp = props => {
     <div id="myModal" data-reveal data-options={buttonToClose} aria-labelledby="modalTitle" aria-hidden="true" role="dialog" className="reveal-modal text-center">
 
       <h3>THIS IS A POPUP!</h3>
+      <QRCode value="https://familynewsletter.herokuapp.com/users/sign_in" />
+
       <button className='general-button' onClick={props.closeFunc}>
         Back to Newsletters
       </button>
