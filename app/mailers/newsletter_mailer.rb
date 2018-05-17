54a9d6efcbbc1aae61cea9101fe1846d206d8704
Galen <galen.h.white@gmail.com>
2018-05-17 15:53:41 -0400

    set up weekly task to test my task understanding

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,13 +1,7 @@
 class NewsletterMailer < ApplicationMailer
-  def send_out(newsletter_title, entries, recipient_info)
-    if Date.today.month == 1
-      @newsletter_month = 12
-      @newsletter_year = Date.today.year - 1
-    else
-      @newsletter_month = Date.today.month - 1
-      @newsletter_year = Date.today.year
-    end
-
+  def send_out(date, recipient_info, entries, newsletter_title)
+    @newsletter_month = date.month
+    @newsletter_year = date.year
     @recipient_name = recipient_info.name
     @entries = entries
     @newsletter_title = newsletter_title
---
+++ app/models/monthly_email.rb
@@ -0,0 +1,35 @@
+class MonthlyEmail
+  def send_out
+    #get all of the newsletters
+    newsletters = Newsletter.all
+
+    #get newsletter's year and month
+    date = get_month_and_year
+
+    #FOR EACH NEWSLETTER:
+    newsletters.each do |newsletter|
+      # get all applicable entries
+      entries = newsletter.formatted_specific_entries(date.year, date.month)
+      # get all subscribers
+      subscribers = newsletter.subscriber_info
+
+      # FOR EACH SUBSCRIBER
+      subscribers.each do |subscriber|
+      # call NewsletterMailer.send_out(newsletter, recipient_info)
+        NewsletterMailer.send_out(date, subscriber, entries, newsletter.title)
+      end
+    end
+  end
+
+  def get_month_and_year
+    if Date.today.month == 1
+      newsletter_month = 12
+      newsletter_year = Date.today.year - 1
+    else
+      newsletter_month = Date.today.month - 1
+      newsletter_year = Date.today.year
+    end
+
+    { year: newsletter_year, month: newsletter_month }
+  end
+end
