3dc500da3330616bac56ef9f0fbdca22494d0a5a
Galen <galen.h.white@gmail.com>
2018-05-17 13:17:49 -0400

    tried using serializer in mailer, didn't work

---
+++ app/mailers/newsletter_mailer.rb
@@ -0,0 +1,21 @@
+class NewsletterMailer < ApplicationMailer
+  def monthly_newsletter(newsletter_id)
+    if Date.today.month == 1
+      newsletter_month = 12
+      newsletter_year = Date.today.year - 1
+    else
+      newsletter_month = Date.today.month - 1
+      newsletter_year = Date.today.year
+    end
+
+
+
+    @newsletter = Newsletter.find(newsletter_id)
+    @recipients = @newsletter.users, serializer: MemberSerializer
+    @entries = @newsletter.entries.with_year_and_month(newsletter_year, newsletter_month)
+
+    # @invite = invite
+    # @email = @invite.email
+    # mail(to: @email)
+  end
+end
