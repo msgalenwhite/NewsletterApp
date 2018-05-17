b63db7ca732a87923d236adae000a4cab3fa1eb9
Galen <galen.h.white@gmail.com>
2018-05-17 14:17:08 -0400

    created mailer for email

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,18 +1,19 @@
 class NewsletterMailer < ApplicationMailer
-  def monthly_newsletter(newsletter_id)
+  def send_out(newsletter_id, recipient_info)
+    newsletter = Newsletter.find(newsletter_id)
+
     if Date.today.month == 1
-      newsletter_month = 12
-      newsletter_year = Date.today.year - 1
+      @newsletter_month = 12
+      @newsletter_year = Date.today.year - 1
     else
-      newsletter_month = Date.today.month - 1
-      newsletter_year = Date.today.year
+      @newsletter_month = Date.today.month - 1
+      @newsletter_year = Date.today.year
     end
 
+    @recipient_name = recipient_info.name
+    @entries = newsletter.formatted_specific_entries(@newsletter_year, @newsletter_month)
+    @newsletter_title = newsletter.title
 
-
-    @newsletter = Newsletter.find(newsletter_id)
-    @recipients = @newsletter.subscriber_info
-
-    entries = @newsletter.entries.with_year_and_month(newsletter_year, newsletter_month)
+    mail(to: recipient_info.email)
   end
 end
