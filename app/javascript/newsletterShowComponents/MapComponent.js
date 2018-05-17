70611631d5cebb741fb270cb8300c43c47c285f1
Galen <galen.h.white@gmail.com>
2018-05-17 17:27:06 -0400

    separated map into its own function

--- app/javascript/newsletterShowComponents/MapComponent.js
+++ app/javascript/newsletterShowComponents/MapComponent.js
@@ -9,6 +9,7 @@ class MapComponent extends Component {
     }
     this.initMap = this.initMap.bind(this)
     this.callMap = this.callMap.bind(this)
+    this.drawMap = this.drawMap.bind(this)
     this.mapLocations = this.mapLocations.bind(this)
   }
 
@@ -19,6 +20,39 @@ class MapComponent extends Component {
   callMap() {
     window.initMap = this.initMap
     loadJS("https://maps.googleapis.com/maps/api/js?key=AIzaSyC668sqglnHBxF-VWPbu3FYTzQfdmXzu2k&callback=initMap")
+  }
+
+  drawMap() {
+    const locations = this.mapLocations()
+
+    let centerStats;
+    if (locations.length > 0) {
+      centerStats = {
+        lat: locations[0].lat,
+        lng: locations[0].lng
+      }
+    } else {
+      centerStats = { lat: 33.8366, lng: 117.9143 }
+    }
+
+    this.map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 9,
+      center: centerStats
+    })
+
+    let markers = []
+
+    locations.forEach((place) => {
+      if (place.lat && place.lng) {
+        let marker = new google.maps.Marker({
+          position: { lat: place.lat, lng: place.lng },
+          label: place.name
+        })
+
+        markers.push(marker)
+        marker.setMap(this.map)
+      }
+    })
   }
 
   initMap() {
@@ -39,37 +73,7 @@ class MapComponent extends Component {
       .then ( response => response.json() )
       .then ( response => {
         this.setState({ users: response })
-
-        const locations = this.mapLocations()
-
-        let centerStats;
-        if (this.state.users.length > 0) {
-          centerStats = {
-            lat: this.state.users[0].latitude,
-            lng: this.state.users[0].longitude
-          }
-        } else {
-          centerStats = { lat: 33.8366, lng: 117.9143 }
-        }
-
-        this.map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 9,
-          center: centerStats
-        })
-
-        let markers = []
-
-        locations.forEach((place) => {
-          if (place.lat && place.lng) {
-            let marker = new google.maps.Marker({
-              position: { lat: place.lat, lng: place.lng },
-              label: place.name
-            })
-
-            markers.push(marker)
-            marker.setMap(this.map)
-          }
-        })
+        this.drawMap()
       })
       .catch ( error => console.error(`Error in fetch: ${error.message}`) );
   }
