3f7e69db3dd601330f1118f3f7403682b5b38d68
Galen <galen.h.white@gmail.com>
2018-05-19 21:53:18 -0400

    moved user tests to correct file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,18 @@ describe User, :type => :model do
     it { should have_valid(:current_state).when("state") }
     it { should_not have_valid(:current_state).when(nil, "") }
   end
+
+  context 'methods' do
+    describe '#full_name' do
+      it { expect(test_user).to respond_to(:full_name) }
+      it { expect(test_user.full_name).to eq("#{test_user.first_name} #{test_user.last_name}") }
+    end
+
+    describe '#name_and_email' do
+      it { expect(test_user).to respond_to(:name_and_email) }
+      it { expect(test_user.name_and_email).to eq(
+        { name: test_user.full_name, email: test_user.email }
+        ) }
+    end
+  end
 end
