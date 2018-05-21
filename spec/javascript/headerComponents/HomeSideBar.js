9bb13c1506551f05a74e477b54b1099b09029d00
Galen <galen.h.white@gmail.com>
2018-05-21 13:42:57 -0400

    tested HomeSideBar

---
+++ spec/javascript/headerComponents/HomeSideBar.js
@@ -0,0 +1,17 @@
+import HomeSideBar from '../../../app/javascript/headerComponents/HomeSideBar'
+
+describe('HomeSideBar', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<HomeSideBar />)
+  })
+
+  it("asks what's next", () => {
+    expect(wrapper.find('h5').text()).toEqual("What's next?")
+  })
+  it("has a link to the newsletters page", () => {
+    expect(wrapper.find('a').props().href).toEqual('/newsletters/new')
+    expect(wrapper.find('a').find('button').text()).toEqual('Start a Newsletter')
+  })
+})
