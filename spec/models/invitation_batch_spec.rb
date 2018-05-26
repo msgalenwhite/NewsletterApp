eb11bb8fb692e4546fdc25072568f0df7b136883
Galen <galen.h.white@gmail.com>
2018-05-26 13:49:20 -0400

    began testing invitation batch

---
+++ spec/models/invitation_batch_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe InvitationBatch do
+  it 'takes in a hash with and creates readable instance variables for invitees and errors'
+
+
+  describe '#allvalid?' do
+    it 'takes in an array of Invitation Objects, and returns a boolean'
+    it 'returns false if one has errors'
+    it 'returns true if none have errors'
+  end
+
+  describe '#dispatch' do
+
+  end
+end
